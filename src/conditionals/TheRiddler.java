1f8cdf555c97b1f17285435e636b56a02f45deae
Fangtastic7 <30578148+Fangtastic7@users.noreply.github.com>
2017-10-13 18:31:37 -0700

    The Riddler, Birthday Reminder, Guessing, and working on guessing pi
    Eclipse projects

--- src/conditionals/TheRiddler.java
+++ src/conditionals/TheRiddler.java
@@ -3,22 +3,35 @@
 
 package conditionals;
 
+import javax.swing.JOptionPane;
+
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+			int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+			String riddle = JOptionPane.showInputDialog("What do you call a sick eagle?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+			if(riddle.equalsIgnoreCase("illegal")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
+			score = score+1;
+			}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+			else {
+				JOptionPane.showMessageDialog(null, "wrong!");
+			}
 		// 6. Add some more riddles
-
+			String riddle2 = JOptionPane.showInputDialog("What do you call an eagle flying over a bay?");
+			
+			if(riddle2.equalsIgnoreCase("bagle")){
+				JOptionPane.showMessageDialog(null, "Right!");
+				score = score+1;
+			}
+			
 		// 2. Make a pop up to show the score.
-		
+			JOptionPane.showMessageDialog(null, "Your score is " + score);
 	}
 }
 
--- src/extra/BirthdayReminder.java
+++ src/extra/BirthdayReminder.java
@@ -2,6 +2,8 @@
 //    Level 0
 package extra;
 
+import javax.swing.JOptionPane;
+
 public class BirthdayReminder {
 	public static void main(String[] args) {
 		
@@ -11,17 +13,38 @@ public class BirthdayReminder {
 		String myBirthday = "April 16th";
 
 		// 2. Find out whose birthday the user wants and and store their response in a variable
-	
+		String answer = JOptionPane.showInputDialog("Whose birthday do you want to know?");
 		// 3. Print out what the user typed
-		
+		if(answer.equalsIgnoreCase("yours")) {
+			JOptionPane.showMessageDialog(null, "You want to know my birthday?");
+		}
+		else {
+		JOptionPane.showMessageDialog(null, "So.... you want to know " + answer + "'s birthday?");
+		}
 		// 4. if user asked for "mom"
 			//print mom's birthday
+			if(answer.equalsIgnoreCase("mom's")){
+				JOptionPane.showMessageDialog(null, momsBirthday);
+				
+			}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+			else if (answer.equalsIgnoreCase("father's")) {
+				JOptionPane.showMessageDialog(null, dadsBirthday);
+				
+			}
 		// 6. if user asked for you
 			// print myBirthday
+			else if(answer.equalsIgnoreCase("yours")) {
+				JOptionPane.showMessageDialog(null, myBirthday);
+				
+				
+			}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+			else {
+				JOptionPane.showMessageDialog(null, "Sorry, I forgot that person's birthday.");
+				System.exit(0);
+			}
 	} 
 
 }
